d99856131b1178bbf018bb0091eaffec23551733
Moutaz Chaara <xmoutaz@hotmail.com>
2022-01-02 17:14:11 +0100

    change env url

--- src/lib/http-loader.ts
+++ src/lib/http-loader.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { TranslateLoader } from "@ngx-translate/core";
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 export class TranslateHttpLoader implements TranslateLoader {
   readonly translations$ = new BehaviorSubject<any>(null);
 
-  readonly baseUrl = 'https://gas-apiv0.herokuapp.com/translations/ngx-translate'
+  readonly baseURL = `${environment.baseUrl}/translations/ngx-translate`;
   constructor(private http: HttpClient) { }
   getTranslation(lang: string) {
-   return this.http.get(`${this.baseUrl}/${lang}`);
+   return this.http.get(`${this.baseURL}/${lang}`);
   
 }
     
