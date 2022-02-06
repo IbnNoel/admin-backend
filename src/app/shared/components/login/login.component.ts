5e92c2804032d801eef32f3c73d72465663c89eb
Moutaz Chaara <xmoutaz@hotmail.com>
2022-02-06 11:55:58 +0100

    fixing login issue

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
       password:['', Validators.compose([
         Validators.required,
         Validators.minLength(8),
-        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+        //Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
       ])],
     })
   }
